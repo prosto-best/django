32d0ef5e83998cc9cc7535ed6f70f5433fa4ee9d
prosto-best <az271004@gmail.com>
2021-03-20 18:43:24 +0300

    Initial commit

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from .  import views
 
-urlpatterns = [path("", view=views.index, name="index")]
+urlpatterns = [path("", view=views.index, name="index"),
+    path("<int:question_id>/", view=views.detail, name="detail"),
+    path("<int:question_id>/results/", view=views.results, name="results"),
+    path("<int:question_id>/vote/",view.views.vote, name="vote"),
+
+]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,24 @@
+from django import template
+from polls.models import Question
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, response
+from django.template import loader
+from django.http import HttpResponse, response, Http404
 
 def index(request):
-    return HttpResponse("Hello! You are here! Here is here!")
+    all_questions = Question.objects.order_by("-publication_date")
+    template = loader.get_template("polls/index.html")
+    context = {"all_questions": all_questions}
+    return render(request, "polls/index.html", context)
 
-# Create your views here.
+def results(request, question_id):
+    response = f"Список ответов на ваши вопросы {question_id} results" 
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    response = f"Ваш ответ на {question_id}"
+    return HttpResponse(response)
+
+def detail(request, question_id):
+    response = f"Вы чиитаете вопрос {question_id}"
+    return HttpResponse(response)
